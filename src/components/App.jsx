b8ea01f20407547300817115ee76addd4698ef3e
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-09-25 19:42:45 +0300

    add task-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import { Profile } from 'components/Profile/Profile';
-import user from 'data/user.json';
+import user from 'data/user';
+
+import { Statistics } from 'components/Statistics/Statistics';
+import data from 'data/data';
 
 export const App = () => {
   return (
@@ -11,6 +14,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { getRandomHexColor } from 'components/RandomeHexColor';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            className={css.item}
+            style={{
+              backgroundColor: getRandomHexColor(),
+            }}
+            key={id}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ).isRequired,
+};
