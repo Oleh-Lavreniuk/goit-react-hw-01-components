9026c7487de93acccc9927670388c0ccc4ad22d0
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-09-29 23:14:32 +0300

    add emotion to first component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,40 @@
 import PropTypes from 'prop-types';
-import css from './Profile.module.css';
+import {
+  ProfileTag,
+  DescriptionWrapper,
+  Avatar,
+  UserName,
+  Info,
+  Stats,
+  Label,
+  Quantity,
+} from './Profile.styled';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div className={css.profile}>
-      <div className={css.descriptionWrapper}>
-        <img src={avatar} alt="User avatar" className={css.avatar} />
-        <p className={css.username}>{username}</p>
-        <p className={css.tag}>@{tag}</p>
-        <p className={css.location}>{location}</p>
-      </div>
+    <ProfileTag>
+      <DescriptionWrapper>
+        <Avatar src={avatar} alt="User avatar" />
+        <UserName>{username}</UserName>
+        <Info>@{tag}</Info>
+        <Info>{location}</Info>
+      </DescriptionWrapper>
 
-      <ul className={css.stats}>
+      <Stats>
         <li>
-          <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{stats.followers}</span>
+          <Label>Followers</Label>
+          <Quantity>{stats.followers}</Quantity>
         </li>
         <li>
-          <span className={css.label}>Views</span>
-          <span className={css.quantity}>{stats.views}</span>
+          <Label>Views</Label>
+          <Quantity>{stats.views}</Quantity>
         </li>
         <li>
-          <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{stats.likes}</span>
+          <Label>Likes</Label>
+          <Quantity>{stats.likes}</Quantity>
         </li>
-      </ul>
-    </div>
+      </Stats>
+    </ProfileTag>
   );
 };
 
--- src/components/Profile/Profile.styled.jsx
+++ src/components/Profile/Profile.styled.jsx
@@ -1,4 +1,6 @@
-.profile {
+import styled from '@emotion/styled';
+
+export const ProfileTag = styled.div`
   width: 300px;
   margin: 0 auto 40px auto;
   font-size: 16px;
@@ -8,14 +10,14 @@
   box-shadow: 8px 4px 4px rgb(106, 165, 82);
   border-radius: 5px;
   transition: transform 300ms cubic-bezier(0.4, 0, 0.2, 1);
-}
 
-.profile:hover,
-.profile:focus {
-  transform: scale(1.05);
-}
+  &:hover,
+  &:focus {
+    transform: scale(1.05);
+  }
+`;
 
-.descriptionWrapper {
+export const DescriptionWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -23,71 +25,62 @@
   border: 1px solid rgba(228, 233, 240);
   border-radius: 5px 5px 0 0;
   padding: 20px;
-}
+`;
 
-.avatar {
+export const Avatar = styled.img`
   object-fit: cover;
   width: 200px;
   height: 200px;
   margin-bottom: 20px;
-}
+`;
 
-.username {
+export const UserName = styled.p`
   font-weight: 700;
   font-size: 20px;
   margin: 0;
   margin-bottom: 10px;
-  padding: 0;
-}
+`;
 
-.tag {
+export const Info = styled.p`
   margin: 0;
   margin-bottom: 10px;
   color: rgb(142, 119, 153);
-}
+`;
 
-.location {
-  margin: 0;
-  margin-bottom: 10px;
-  padding: 0;
-  color: rgb(142, 119, 153);
-}
-
-.stats {
+export const Stats = styled.ul`
   display: flex;
-  font-family: 'Roboto', sans-serif;
   font-weight: 600;
   width: 100%;
   margin: 0;
   padding: 0;
   list-style: none;
-}
 
-.stats > li {
-  display: flex;
-  font-weight: 400;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  padding: 10px 10px;
-  background-color: #f7f7f7;
-  border: 1px solid rgba(228, 233, 240);
-}
+  & > li {
+    display: flex;
+    font-weight: 400;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    padding: 10px 10px;
+    background-color: #f7f7f7;
+    border: 1px solid rgba(228, 233, 240);
+  }
 
-.stats > li:first-child {
-  border-radius: 0 0 0 5px;
-}
-.stats > li:last-child {
-  border-radius: 0 0 5px 0;
-}
+  & > li:first-child {
+    border-radius: 0 0 0 5px;
+  }
+  & > li:last-child {
+    border-radius: 0 0 5px 0;
+  }
+`;
 
-.label {
+export const Label = styled.span`
   font-size: 14px;
   color: rgb(142, 119, 153);
-}
+`;
 
-.quantity {
+export const Quantity = styled.span`
   color: black;
   font-weight: 700;
   margin-top: 5px;
-}
+`;
